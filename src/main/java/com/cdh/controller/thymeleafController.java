343d04c6cc2bfd73d274d136e5481c6aa4753467
cdh211 <317675086@qq.com>
2020-03-29 10:54:57 +0800

    11.txtback

---
+++ src/main/java/com/cdh/controller/thymeleafController.java
@@ -0,0 +1,12 @@
+package com.cdh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class thymeleafController {
+//    @RequestMapping("/index.html")
+//    public String index(){
+//        return "index";
+//    }
+}
